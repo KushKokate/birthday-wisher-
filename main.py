6b467c35352a41d08b07d00a915d5dfcbe881816
Kush <kokatekush@gmail.com>
2024-03-10 23:51:15 +0530

    pandas, dict comprehension

--- main.py
+++ main.py
@@ -3,37 +3,16 @@ import smtplib
 import datetime
 import pandas as pd
 import datetime as dt
-# 1. Update the birthdays.csv with your friends & family's details.
-# HINT: Make sure one of the entries matches today's date for testing purposes. e.g.
-#name,email,year,month,day
-#YourName,your_own@email.com,today_year,today_month,today_day
+import random
 
-# 2. Check if today matches a birthday in the birthdays.csv
-# HINT 1: Create a tuple from today's month and day using datetime. e.g.
-# today = (today_month, today_day)
 now=dt.datetime.now()
-today = (now.month, now.day, now.year, now.hour, now.minute, now.second)
-# HINT 2: Use pandas to read the birthdays.csv
+today_tuple = (now.month, now.day, now.year, now.hour, now.minute, now.second)
 data = pd.read_csv('birthdays.csv')
-print(data)
-# HINT 3: Use dictionary comprehension to create a dictionary from birthday.csv that is formated like this:
-# birthdays_dict = {
-#     (birthday_month, birthday_day): data_row
-# }
-new_dict = {:month for (month, day) in data}
-print(new_dict)
-#Dictionary comprehension template for pandas DataFrame looks like this:
-# new_dict = {new_key: new_value for (index, data_row) in data.iterrows()}
-#e.g. if the birthdays.csv looked like this:
-# name,email,year,month,day
-# Angela,angela@email.com,1995,12,24
-#Then the birthdays_dict should look like this:
-# birthdays_dict = {
-#     (12, 24): Angela,angela@email.com,1995,12,24
-# }
 
-#HINT 4: Then you could compare and see if today's month/day tuple matches one of the keys in birthday_dict like this:
-# if (today_month, today_day) in birthdays_dict:
+birthdays_dict = {(data_row["month"],data_row["day"]):data_row for (index, data_row) in data.iterrows()}
+
+if today_tuple in birthdays_dict:
+    file_path = f"letter_templates/letter_{random.randint(0,3)}.txt"
 
 # 3. If there is a match, pick a random letter (letter_1.txt/letter_2.txt/letter_3.txt) from letter_templates and replace the [NAME] with the person's actual name from birthdays.csv
 # HINT 1: Think about the relative file path to open each letter. 
